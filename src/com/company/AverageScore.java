aeb9e65781a91a3e6fddbeb27a3ae7db0df38355
sujeebadal <sujeebadal@gmail.com>
2019-01-29 12:04:22 -0500

    changes

--- src/com/company/AverageScore.java
+++ src/com/company/AverageScore.java
@@ -7,17 +7,17 @@ public class AverageScore {
     public static void main(String[] args) {
         Scanner Keyboard = new Scanner(System.in);
         System.out.println("Enter a score: ");
-        int score1= Keyboard.nextInt();
-        int score2= Keyboard.nextInt();
-        int score3= Keyboard.nextInt();
-        int score4= Keyboard.nextInt();
-        int score5= Keyboard.nextInt();
-        int score6= Keyboard.nextInt();
-        int score7= Keyboard.nextInt();
-        int score8= Keyboard.nextInt();
-        int score9= Keyboard.nextInt();
-        int score10= Keyboard.nextInt();
-        int average= score1+score2+score3+score4+score5+score6+score7+score8+score9+score10;
+        double score1= Keyboard.nextInt();
+        double score2= Keyboard.nextInt();
+        double score3= Keyboard.nextInt();
+        double score4= Keyboard.nextInt();
+        double score5= Keyboard.nextInt();
+        double score6= Keyboard.nextInt();
+        double score7= Keyboard.nextInt();
+        double score8= Keyboard.nextInt();
+        double score9= Keyboard.nextInt();
+        double score10= Keyboard.nextInt();
+        double average=(score1+score2+score3+score4+score5+score6+score7+score8+score9+score10)/10;
         System.out.println(average);
 
     }
